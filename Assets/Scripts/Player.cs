2abc5b15271704a4e765708ea38b678768740e7f
TonyInfinity <quachhengtony@gmail.com>
2023-03-12 09:18:13 +0700

    add lives system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
@@ -9,6 +11,12 @@ public class Player : MonoBehaviour
     public float jumpForce = 8f;
     public float runForce = 5f;
     public float gravity = 9.81f * 2f;
+    public int lives = 3;
+
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private TextMeshProUGUI txtLives;
 
     private void Awake()
     {
@@ -20,6 +28,12 @@ public class Player : MonoBehaviour
         direction = Vector3.zero;
     }
 
+    private void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        //txtLives = this.GetComponent<TMP_Text>();
+    }
+
     private void Update()
     {
         direction += Vector3.down * gravity * Time.deltaTime;
@@ -28,7 +42,8 @@ public class Player : MonoBehaviour
         {
             direction = Vector3.down;
 
-            if (Input.GetButton("Jump")) {
+            if (Input.GetButton("Jump"))
+            {
                 direction = Vector3.up * jumpForce;
             }
 
@@ -46,11 +61,48 @@ public class Player : MonoBehaviour
         character.Move(direction * Time.deltaTime);
     }
 
+    private int ite = 4;
+
+    private void RedDamageEffect()
+    {
+        spriteRenderer.color = Color.red;
+        InvokeRepeating("ResetColor", 0.25f, 0.5f);
+    }
+
+    private void ResetColor()
+    {
+        spriteRenderer.color = Color.white;
+        ite--;
+        if (ite == 0)
+        {
+            CancelInvoke();
+            ite = 4;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Left") || other.CompareTag("Right") || other.CompareTag("Meteor")) {
-            FindObjectOfType<GameManager>().GameOver();
+        if (other.CompareTag("Left") || other.CompareTag("Right") || other.CompareTag("Meteor"))
+        {
+            InvokeRepeating("RedDamageEffect", 0f, 0.5f);
+            spriteRenderer.color = Color.white;
+            if (lives > 1)
+            {
+                ite = 4;
+                lives -= 1;
+                txtLives.text = lives.ToString();
+                return;
+            }
+            else
+            {
+                lives = 3;
+                spriteRenderer.color = Color.white;
+
+                txtLives.text = lives.ToString();
+                FindObjectOfType<GameManager>().GameOver();
+            }
         }
+
     }
 
 }
